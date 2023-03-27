54186dafe971fa79dfb2480ad82960c8b621bec5
Billy Lowry <billyn.lowry@gmail.com>
2023-03-27 11:23:55 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,6 @@
+// Divide using arrow functions. 
+const divide = (divide) => 2000 / 100
+// Square using arrow functions.
+const square = (square) => 2 ** 2
+// Add using arrow functions.
+const add = (add) => 3 + 4; 
